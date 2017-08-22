ad029855c164202a4dca9a62bfcfa099316ef871
saresq <santiago.aresq@gmail.com>
2017-08-22 13:43:34 -0300

    css

--- js/embdmap.js
+++
@@ -1,7 +0,0 @@
-$('.maps').click(function () {
-    $('.maps iframe').css("pointer-events", "auto");
-});
-
-$( ".maps" ).mouseleave(function() {
-  $('.maps iframe').css("pointer-events", "none");
-});
